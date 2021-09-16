bcb27ebefa1dacd15ba3ba73204a6675ce07ace4
Alif Corrêa Flores <alif.flores@credpago.com>
2021-09-16 10:15:11 -0300

    ajustes finos

--- cypress/integration/API.spec.js
+++ cypress/integration/API.spec.js
@@ -2,30 +2,30 @@
 
 const btoa = require('btoa');
 
-describe('grupo de testes', ()=>{
-
-    const data1 = {
+const data = {
+    fef: {
         id_imovel: '6760',
         imob_status: 'Ativo'
-    };
+    },
+    relatorio: {
+        nome: 'Alif',
+        cpf: '09129.2292109',
+        email: 'alif.flores@credpago.com'
+    }
+};
 
-    it('Teste 1 - API',()=>{
+describe('Testes com API', ()=>{
+
+    it('API sem Auth',()=>{
         cy.request('GET', '/blacklist/fef/'+ btoa(6760)).then((response)=>{
             expect(response.status).to.eq(200);
             expect(response.body).is.not.null;
-            expect(response.body.data.id).to.eq(data1.id_imovel);
-            expect(response.body.data.imovel[0].imob_status).is.eq(data1.imob_status);
+            expect(response.body.data.id).to.eq(data.fef.id_imovel);
+            expect(response.body.data.imovel[0].imob_status).is.eq(data.fef.imob_status);
         });
     });
 
     it('API com Auth', ()=>{
-
-        const data2 = {
-            nome: 'Alif',
-            cpf: '09129.2292109',
-            email: 'alif.flores@credpago.com'
-        };
-
         cy.request({
             method: 'POST',
             url: '/blacklist/relatorio',
@@ -33,9 +33,9 @@ describe('grupo de testes', ()=>{
                 user: 'api@credpago',
                 pass: 'Z7tmjxLUlax9ELvQ'
             },
-            body: {
-                data2
-            }
-        })
+            // body: {
+            //    data
+            // }
+        });
     });
 });
